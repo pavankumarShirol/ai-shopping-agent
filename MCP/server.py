74761c8a17afe74db819556893fcb196eda1cbc5
pavankumarShirol <pavankumarshirol03@gmail.com>
2026-02-25 23:40:54 +0530

    update MCP code to adjust for deployment of mcp server

--- MCP/server.py
+++ MCP/server.py
@@ -4,13 +4,23 @@ import asyncio
 import httpx
 from fastmcp import FastMCP
 
+BACKEND_BASE_URL = "https://ai-shopping-agent-el0o.onrender.com"
+
 async def main():
-    async with httpx.AsyncClient() as spec_client:
-        resp = await spec_client.get("http://localhost:4000/api-docs.json")
+    
+    timeout = httpx.Timeout(
+    connect=30.0,
+    read=60.0,
+    write=30.0,
+    pool=30.0,
+   )
+    
+    async with httpx.AsyncClient(timeout=timeout) as spec_client:
+        resp = await spec_client.get(f"{BACKEND_BASE_URL}/api-docs.json")
         resp.raise_for_status()
         openapi_spec = resp.json()
 
-    api_client = httpx.AsyncClient(base_url="http://localhost:4000")
+    api_client = httpx.AsyncClient(base_url=BACKEND_BASE_URL, timeout=timeout)
 
     mcp = FastMCP.from_openapi(
         openapi_spec=openapi_spec,
